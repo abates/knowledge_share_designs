b4d144c7b8d7465f6bd5a25cd2ee90240208108a
Andrew Bates <andrew.bates@networktocode.com>
2022-10-12 16:15:21 -0400

    Updates

---
+++ designs/ks_initial_design.py
@@ -0,0 +1,10 @@
+from design_builder.design_builder import DesignJob
+
+from .context import KSInitialDesignContext
+
+class KSInitialDesign(DesignJob):
+    class Meta:
+        name = "Initial Data"
+        commit_default = False
+        design_file = "templates/ks_initial_design.yaml.j2"
+        context_class = KSInitialDesignContext
